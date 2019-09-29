9d42ec71cffa5e0707fec3e2428bc84b5f51f9bf
nakkaa <tnakkaa@gmail.com>
2019-09-29 20:48:12 +0900

    Added future(get weather infomation).

--- plugins/hato.py
+++ plugins/hato.py
@@ -4,27 +4,43 @@ from slackbot.bot import listen_to      # チャネル内発言で反応する�
 from slackbot.bot import default_reply  # 該当する応答がない場合に反応するデコーダ
 import unicodedata
 from logging import getLogger
+from library.weather import get_city_id_from_city_name
+from library.weather import get_weather
+
 logger = getLogger(__name__)
 
 # 「hato help」を見つけたら、使い方を表示する
-@listen_to('hato help')
+@respond_to('help')
 def help(message):
     user = message.user['name']
     logger.debug("%s called 'hato help'", user)
     message.send(
         '\n使い方\n'
         '```'
-        'hato help       botの使い方を表示する。\n'
-        'hato >< [文字列] 文字列を吹き出しで表示する。\n'
+        'help       ... botの使い方を表示する。\n'
+        '天気 [地名] ... 地名の天気予報を表示する。\n'
+        '>< [文字列] ... 文字列を吹き出しで表示する。\n'
         '```')
 
+@respond_to('天気 .+')
+def weather(message):
+    user = message.user['name']
+    logger.debug("%s called 'hato 天気'", user)
+    text = message.body['text']
+    tmp, word = text.split(' ', 1)
+    city_id = get_city_id_from_city_name(word)
+    if city_id == None:
+        message.send('該当する情報が見つからなかったっぽ！')
+    else:
+        weather_info = get_weather(city_id)
+        message.send('```' + weather_info +'```')
+
 # 「hato >< 文字列」を見つけたら、文字列を突然の死で装飾する
-@listen_to('^hato &gt;&lt; .+')
+@respond_to('^&gt;&lt; .+')
 def totuzensi(message):
     user = message.user['name']
     text = message.body['text']
-    word_list = text.split(' ', 2)
-    word = str(word_list[2])
+    tmp, word = text.split(' ', 1)
     logger.debug("%s called 'hato >< %s'", user, word)
     word = generator(word)
     msg = '\n```' + word + '```'
