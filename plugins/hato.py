71eee673f9f5f2f74f797845c1f09f01739db91c
nakkaa <tnakkaa@gmail.com>
2019-09-29 11:04:38 +0900

    Added license text

--- plugins/hato.py
+++ plugins/hato.py
@@ -4,11 +4,12 @@ from slackbot.bot import respond_to     # @botname: で反応するデコーダ
 from slackbot.bot import listen_to      # チャネル内発言で反応するデコーダ
 from slackbot.bot import default_reply  # 該当する応答がない場合に反応するデコーダ
 
-@respond_to('メンション')
-def mention_func(message):
-    message.reply('私にメンションと言ってどうするのだ') # メンション
-
-@listen_to('リッスン')
+@listen_to('hato help')
 def listen_func(message):
-    message.send('誰かがリッスンと投稿したようだ')      # ただの投稿
-    message.reply('君だね？')  
+    #message.send('誰かがリッスンと投稿したようだ')      # ただの投稿
+    message.reply(
+        '\n使い方\n'
+        '```'
+        'hato help       botの使い方を表示する。\n'
+        'hato >< [文字列] 吹き出しで表示する。\n'
+        '```')
