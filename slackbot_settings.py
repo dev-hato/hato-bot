47d35e7de49795c3b2bd0b617cdf1e4cf93ef617
nakkaa <tnakkaa@gmail.com>
2020-01-19 20:36:03 +0900

    Configファイルにコメントを追加

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -7,15 +7,15 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 # SlackのAPI Tokenを指定する。
 API_TOKEN = str(os.environ['SLACKBOT_API_TOKEN'])
-# データベースの接続情報
-# postgres://user:pwd@host:port/db
+# データベースの接続情報(URL形式)
+# postgres://user:password@host:port/dbname
 db_auth = urllib.parse.urlparse(str(os.environ['DATABASE_URL']))
-DB_HOST = db_auth.hostname or 'localhost'
-DB_USER = db_auth.username or 'root'
-DB_PASSWORD = db_auth.password or ''
-DB_PORT = db_auth.port or 5432
+DB_HOST = db_auth.hostname
+DB_USER = db_auth.username
+DB_PASSWORD = db_auth.password
+DB_PORT = db_auth.port
 DB_NAME = db_auth.path[1:]
-DB_SSL = bool(db_auth.path[1:]) #DB_NAMEが空の場合はSSLを無効にする(for Develop)
+DB_SSL = bool(db_auth.path[1:]) #DB_NAMEが空の場合はSSLを無効にする(for Develop)。Herokuの場合はTrue。
 
 # Slack bot用の設定
 DEFAULT_REPLY = "使い方がわからない時は `help` とメンションするっぽ!"
