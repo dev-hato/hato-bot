f5d71c5c8bc5e011187a3a2f0e5c1d3dab6d6e7e
Goryudyuma <umaretekyoumade@gmail.com>
2020-06-06 02:44:30 +0900

    test_hato.pyのlint対応

--- tests/plugins/test_hato.py
+++ tests/plugins/test_hato.py
@@ -1,3 +1,7 @@
+"""
+hato.pyのテスト
+"""
+
 import re
 import unittest
 from unittest.mock import MagicMock
@@ -8,19 +12,25 @@ from plugins.hato import split_command, respond_to_with_space
 
 
 class TestRespondToWithSpace(unittest.TestCase):
+    """
+    スペースが含まれているときのテスト
+    """
     @staticmethod
     def func():
+        """ init """
         func = MagicMock()
         func.__name__ = 'test'
         return func
 
     def test_normal(self):
+        """ 通常パターン """
         func = self.func()
         respond_to_with_space(r'^amesh kyoko$')(func)
         pattern = re.compile(r'^\s*amesh\s*kyoko$')
         self.assertEqual(PluginsManager.commands['respond_to'][pattern], func)
 
     def test_flag(self):
+        """ 改行が含まれる場合のマッチフラグが入ったパターン """
         func = self.func()
         flag = re.DOTALL
         respond_to_with_space(r'^amesh kyoko$', flag)(func)
@@ -29,25 +39,36 @@ class TestRespondToWithSpace(unittest.TestCase):
 
 
 class TestSplitCommand(unittest.TestCase):
+    """
+    split commandのテスト
+    """
+
     def test_only_hankaku_space(self):
+        """ 半角スペースのみのテストケース """
         self.assertEqual(split_command(' 天気 東京'), ['天気', '東京'])
 
     def test_only_zenkaku_space(self):
+        """ 全角スペースのみのテストケース """
         self.assertEqual(split_command('　天気　東京'), ['天気', '東京'])
 
     def test_hankaku_and_zenkaku_space(self):
+        """ 半角スペースと全角スペースが混ざったテストケース1 """
         self.assertEqual(split_command(' 天気　東京'), ['天気', '東京'])
 
     def test_zenkaku_and_hankaku_space(self):
+        """ 半角スペースと全角スペースが混ざったテストケース2 """
         self.assertEqual(split_command('　天気 東京'), ['天気', '東京'])
 
     def test_last_hankaku_space(self):
+        """ 行末に半角スペースが入ったテストケース """
         self.assertEqual(split_command(' 天気 東京 '), ['天気', '東京'])
 
     def test_last_zenkaku_space(self):
+        """ 行末に全角スペースが入ったテストケース """
         self.assertEqual(split_command(' 天気 東京　'), ['天気', '東京'])
 
     def test_maxsplit(self):
+        """ 空白が間に2つ以上ある場合 """
         self.assertEqual(split_command(' text add テスト', 1),
                          ['text', 'add テスト'])
 
