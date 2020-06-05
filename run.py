545adb7f6ea44722afee29c2d3020b3ccab929cf
Goryudyuma <umaretekyoumade@gmail.com>
2020-06-06 02:48:07 +0900

    run.pyのlint対応

--- run.py
+++ run.py
@@ -1,19 +1,25 @@
 # coding: utf-8
+
+"""
+BotのMain関数
+"""
 import sys
 import logging
 import logging.config
-from slackbot import settings
 from slackbot.bot import Bot
 
 
 def main():
-    kw = {
+    """
+    Botを起動します
+    """
+    log_format_config = {
         'format': '[%(asctime)s] %(message)s',
         'datefmt': '%Y-%m-%d %H:%M:%S',
         'level': logging.DEBUG,
         'stream': sys.stdout,
     }
-    logging.basicConfig(**kw)
+    logging.basicConfig(**log_format_config)
     logging.getLogger('requests.packages.urllib3.connectionpool').setLevel(
         logging.WARNING)
     bot = Bot()
