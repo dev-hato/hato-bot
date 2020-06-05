9bc30e377dc33d61aca89ad1cc1479aecf31ee2a
Goryudyuma <umaretekyoumade@gmail.com>
2020-06-06 02:51:57 +0900

    test_hatokaraage.pyのlint対応

--- tests/library/test_hatokaraage.py
+++ tests/library/test_hatokaraage.py
@@ -1,13 +1,26 @@
+"""
+鳩は唐揚げのテスト
+"""
+
 import unittest
 from library.hatokaraage import hato_ha_karaage
 
 
 class TestHatoHaKaraage(unittest.TestCase):
+    """
+    鳩を確実に唐揚げにするテスト
+    """
 
     def test_normal(self):
+        """
+        鳩じゃないものは唐揚げにはならない
+        """
         self.assertEqual(hato_ha_karaage('hoge'), 'hoge')
 
     def test_include_hato(self):
+        """
+        鳩であれば唐揚げになる
+        """
         self.assertEqual(hato_ha_karaage('鳩は唐揚げではない'), '鳩は唐揚げ')
 
 
