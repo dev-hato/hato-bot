10ca2b985b59cb0b8c8145051b423134cf2ef5a2
massongit <suzukimasaya428@gmail.com>
2020-06-06 18:09:03 +0900

    Move method

--- create_env.py
+++ create_env.py
@@ -5,29 +5,10 @@
 from library.database import Database
 
 
-class CreateEnvDatabase(Database):
-    """
-    DBのテーブル作成用スクリプト
-    初回環境構築時のみ必要
-    """
-
-    def execute_sql(self, sql: str) -> None:
-        """SQLを実行する"""
-
-        with self.conn.cursor() as cursor:
-            try:
-                cursor.execute(sql)
-                self.conn.commit()
-                print('Create table. {}'.format(sql))
-            except Exception as _e:
-                print('Can not execute sql(create_table).')
-                raise _e
-
-
 def create_table() -> None:
     """テーブルを作成する"""
 
-    with CreateEnvDatabase() as _db, open('setup/pgsql-init/02_init.sql') as init_sql:
+    with Database() as _db, open('setup/pgsql-init/02_init.sql') as init_sql:
         for line in init_sql.readlines():
             _db.execute_sql(line)
 
