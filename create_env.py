51c38407bbe2630ef3b36ad7d40a2e2e29dde948
massongit <suzukimasaya428@gmail.com>
2020-06-06 18:01:51 +0900

    Make common DB

--- create_env.py
+++ create_env.py
@@ -2,33 +2,14 @@
 初回動かす必要のあるスクリプト
 """
 
-import pg8000
-import slackbot_settings as conf
+from library.database import Database
 
 
-class CreateEnvDatabase:
+class CreateEnvDatabase(Database):
     """
     DBのテーブル作成用スクリプト
     初回環境構築時のみ必要
     """
-
-    def __init__(self):
-        try:
-            pg8000.paramstyle = 'qmark'
-            self.conn = pg8000.connect(
-                host=conf.DB_HOST,
-                user=conf.DB_USER,
-                password=conf.DB_PASSWORD,
-                port=conf.DB_PORT,
-                ssl_context=conf.DB_SSL,
-                database=conf.DB_NAME
-            )
-        except:
-            print('Can not connect to database.')
-            raise
-
-    def __enter__(self):
-        return self
 
     def execute_sql(self, sql: str) -> None:
         """SQLを実行する"""
@@ -41,9 +22,6 @@ class CreateEnvDatabase:
             except Exception as _e:
                 print('Can not execute sql(create_table).')
                 raise _e
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.conn.close()
 
 
 def create_table() -> None:
