3047c2198c88e463640e3f9c85bbcc0a22fad0c6
fono09 <fono@fono.jp>
2020-06-07 17:04:59 +0900

    開発用コンテナとしても利用できるようにする

--- setup/hato-bot/entrypoint.sh
+++ setup/hato-bot/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+# PostgreSQL起動待機
 RETRIES=10
 until psql $DATABASE_URL -c "select 1" > /dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
   echo "Waiting for postgres server, $((RETRIES-=1)) remaining attempts..."
   sleep 1
 done
 
-python create_env.py
+pipenv run python create_env.py
 
-python run.py
+pipenv run python run.py
