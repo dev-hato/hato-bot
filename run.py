518300091188ed653cff7cfb266247f038d59525
Goryudyuma <umaretekyoumade@gmail.com>
2020-06-15 03:06:53 +0900

    lint対応でコメント書いた

--- run.py
+++ run.py
@@ -6,10 +6,10 @@ BotのMain関数
 import sys
 import logging
 import logging.config
-from slackeventsapi import SlackEventAdapter
-import slackbot_settings as conf
 import re
+from slackeventsapi import SlackEventAdapter
 from slack import WebClient
+import slackbot_settings as conf
 import plugins.hato as hato
 
 
@@ -28,21 +28,27 @@ logging.getLogger('requests.packages.urllib3.connectionpool').setLevel(
 
 
 class SlackClient:
+    """
+    Slackを操作するClient
+    """
     def __init__(self, channel, send_user):
         self.client = WebClient(token=conf.SLACK_API_TOKEN)
         self.slack_channel = channel
         self.send_user = send_user
 
     def post(self, message):
+        """Slackにポストする"""
         self.client.chat_postMessage(
             channel=self.slack_channel,
             text=message
         )
 
     def get_send_user(self):
+        """botを呼び出したユーザーを返す"""
         return self.send_user
 
     def get_type(self):
+        """slack"""
         return 'slack'
 
 
