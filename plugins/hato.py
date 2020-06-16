921e472361a48a722a2477c250eae7ea63814f6c
Goryudyuma <umaretekyoumade@gmail.com>
2020-06-17 04:47:49 +0900

    ameshから画像を取り出す

--- plugins/hato.py
+++ plugins/hato.py
@@ -203,8 +203,7 @@ def amesh(client: BaseClient):
     if req.status_code == 200:
         with open(f_name, 'wb') as weather_map_file:
             weather_map_file.write(req.content)
-
-    client.upload(f_name, "amesh")
+        client.upload(content=req.content, filename="amesh.jpg")
 
     if os.path.exists(f_name):
         os.remove(f_name)
