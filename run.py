a584350a565f0d5d173e3ac0d910e317a0742b0c
Goryudyuma <umaretekyoumade@gmail.com>
2020-06-21 02:02:25 +0900

    関数分割した

--- run.py
+++ run.py
@@ -30,33 +30,39 @@ def __init__():
         logging.WARNING)
 
 
-def analyze_slack_message(messages: List[dict]) -> Callable[[BaseClient], None]:
+def analyze_message(message: str) -> Callable[[BaseClient], None]:
     """コマンド解析"""
+
+    conditions = {'help': lambda m: hato.help_message,
+                  'eq': lambda m: hato.earth_quake,
+                  '地震': lambda m: hato.earth_quake,
+                  'in': lambda m: hato.labotter_in,
+                  'rida': lambda m: hato.labotter_rida,
+                  'text list': lambda m: hato.get_text_list,
+                  'text add ': lambda m: hato.add_text(m[len('text add '):]),
+                  'text show ': lambda m: hato.show_text(m[len('text show '):]),
+                  'text delete ': lambda m: hato.delete_text(m[len('text delete '):]),
+                  'text random': lambda m: hato.show_random_text,
+                  'text': lambda m: hato.show_random_text,
+                  '天気': lambda m: hato.weather((m[len('天気'):]).strip()),
+                  '>< ': lambda m: hato.totuzensi(m[len('>< '):]),
+                  'amesh': lambda m: hato.amesh,
+                  'amesh ': lambda m: hato.amesh_with_gis((m[len('amesh '):]).strip()),
+                  'version': lambda m: hato.version,
+                  }
+
+    for key, method in conditions.items():
+        if message.startswith(key):
+            return method(message)
+    return hato.default_action
+
+
+def analyze_slack_message(messages: List[dict]) -> Callable[[SlackClient], None]:
+    """Slackコマンド解析"""
 
     if len(messages) > 0 and messages[0]['type'] == 'text':
         message = messages[0]['text'].strip()
-
-        conditions = {'help': lambda m: hato.help_message,
-                      'eq': lambda m: hato.earth_quake,
-                      '地震': lambda m: hato.earth_quake,
-                      'in': lambda m: hato.labotter_in,
-                      'rida': lambda m: hato.labotter_rida,
-                      'text list': lambda m: hato.get_text_list,
-                      'text add ': lambda m: hato.add_text(m[len('text add '):]),
-                      'text show ': lambda m: hato.show_text(m[len('text show '):]),
-                      'text delete ': lambda m: hato.delete_text(m[len('text delete '):]),
-                      'text random': lambda m: hato.show_random_text,
-                      'text': lambda m: hato.show_random_text,
-                      '天気': lambda m: hato.weather((m[len('天気'):]).strip()),
-                      '>< ': lambda m: hato.totuzensi(m[len('>< '):]),
-                      'amesh': lambda m: hato.amesh,
-                      'amesh ': lambda m: hato.amesh_with_gis((m[len('amesh '):]).strip()),
-                      'version': lambda m: hato.version,
-                      }
-
-        for key, method in conditions.items():
-            if message.startswith(key):
-                return method(message)
+        return analyze_message(message)
 
     return hato.default_action
 
