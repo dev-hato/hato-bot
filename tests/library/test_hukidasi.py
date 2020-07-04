3c552f14157bd7fe2c5219c6711be1a0502e6317
fono09 <fono@fono.jp>
2020-07-05 01:10:20 +0900

    テストを追加

---
+++ tests/library/test_hukidasi.py
@@ -0,0 +1,34 @@
+import unittest
+import textwrap
+from library.hukidasi import generator
+
+class TestHukidashiGenerator(unittest.TestCase):
+    """
+    突然の死を生成するテスト
+    """
+
+    def test_normal_one_line_ascii(self):
+        """
+        英数字のみの1行のテキストに対して突然の死が生成される
+        """
+        self.assertEqual(generator('Hello, pegion!'), textwrap.dedent('''\
+        ＿人人人人人人人人人＿
+        ＞  Hello, pegion!  ＜
+        ￣^Y^Y^Y^Y^Y^Y^Y^Y^Y￣'''))
+
+    def test_normal_one_line_zenkaku(self):
+        """
+        全角を含む1行のテキストに対して突然の死が生成される
+        """
+        self.assertEqual(generator('言うことを聞け'), textwrap.dedent('''\
+        ＿人人人人人人人人人＿
+        ＞  言うことを聞け  ＜
+        ￣^Y^Y^Y^Y^Y^Y^Y^Y^Y￣'''))
+
+    def test_normal_multiline(self):
+        """
+        複数行のテキストに対して突然の死が生成される
+        """
+        self.assertEqual(generator('そして、\n鳩は唐揚げになる'), '''\
+        ''')
+
