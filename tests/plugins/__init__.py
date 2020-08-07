15381b76dd30f2eb1af25d0be9406ec6921efb60
massongit <suzukimasaya428@gmail.com>
2020-08-07 20:59:57 +0900

    型ヒント記述

--- tests/plugins/__init__.py
+++ tests/plugins/__init__.py
@@ -11,28 +11,28 @@ class TestClient(BaseClient):
     """
 
     def __init__(self):
-        self.post_message = ""
-        self.filename = ""
+        self.post_message: str = ""
+        self.filename: str = ""
 
     def post(self, message: str):
         self.post_message = message
 
-    def get_post_message(self):
+    def get_post_message(self) -> str:
         """投稿される文章を返す"""
 
         return self.post_message
 
     @staticmethod
-    def get_send_user_name():
+    def get_send_user_name() -> str:
         return "test user"
 
     @staticmethod
-    def get_send_user():
+    def get_send_user() -> str:
         return "abc123"
 
     def upload(self, file, filename):
         self.filename = filename
 
-    def get_filename(self):
+    def get_filename(self) -> str:
         """アップロードする画像のファイル名を返す"""
         return self.filename
