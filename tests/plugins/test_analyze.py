11730951da8774558e40866fcf339d4444ab43a9
massongit <suzukimasaya428@gmail.com>
2020-08-07 09:11:02 +0900

    Add test

--- tests/plugins/test_analyze.py
+++ tests/plugins/test_analyze.py
@@ -6,35 +6,7 @@ import unittest
 
 from plugins.analyze import analyze_message
 import plugins.hato as hato
-from library.clientclass import BaseClient
-
-
-class TestClient(BaseClient):
-    """
-    モッククライアント
-    """
-
-    def __init__(self):
-        self.post_message = ""
-
-    def post(self, message: str):
-        self.post_message = message
-
-    def get_post_message(self):
-        """投稿される文章を返す"""
-
-        return self.post_message
-
-    @staticmethod
-    def get_send_user_name():
-        return "test user"
-
-    @staticmethod
-    def get_send_user():
-        return "abc123"
-
-    def upload(self, file, filename):
-        return
+from tests.plugins import TestClient
 
 
 class TestAnaryzeMessage(unittest.TestCase):
