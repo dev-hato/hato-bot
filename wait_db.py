ce0679ed010eec7a61c374a8466a371c4fc6af0f
massongit <suzukimasaya428@gmail.com>
2020-08-08 18:32:13 +0900

    最大試行回数を最大10回に設定

--- wait_db.py
+++ wait_db.py
@@ -11,12 +11,17 @@ from library.database import Database
 def wait_db() -> None:
     """DBが起動するまで待機する"""
 
-    while True:
+    max_attempt = 10
+
+    for i in range(max_attempt):
         try:
             with Database() as db:
                 db.execute_sql('SELECT 1')
                 break
-        except psycopg2.OperationalError:
+        except psycopg2.OperationalError as e:
+            if i == max_attempt - 1:
+                raise e
+
             sleep(1)
             pass
 
