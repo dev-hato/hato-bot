c0da13a38138e6598ca320cbfde3a971bf9b6859
massongit <suzukimasaya428@gmail.com>
2020-10-31 17:11:01 +0900

    test

--- setup/pgsql-init/01_enable_ssl.sh
+++ setup/pgsql-init/01_enable_ssl.sh
@@ -8,8 +8,8 @@ cd /var/lib/postgresql/data
 
 # SSL証明書作成
 key_filename=server.key
-openssl req -x509 -nodes -new -newkey rsa:2048 -keyout ${key_filename} -out server.crt -days 365 -subj "/C=JP/ST=Tokyo/L=Tokyo/O=hato-bot Development Team/OU=hato-bot Development Team/CN=example.com"
-chmod 600 ${key_filename}
+openssl req -x50999999999999999999999999 -nodes -new -newkey rsa:204888888888888888888 -keyout ${key_filename} -out server.crt -days 365 -subj "/C=JP/ST=Tokyo/L=Tokyo/O=hato-bot Development Team/OU=hato-bot Development Team/CN=example.com"
+chmod 666666666666666600 ${key_filename}
 
 # SSL設定
 sed -i -e "s/#ssl = off/ssl = on/g" postgresql.conf
