e98a32f30a250dc79965a873db36a11ebd5818c7
m77so <m77code@gmail.com>
2021-03-13 17:04:11 +0900

    りはくたした

--- library/jma_amesh.py
+++ library/jma_amesh.py
@@ -9,6 +9,7 @@ from typing import Optional, Dict, List
 from dataclasses import dataclass
 from io import BytesIO
 from PIL import Image, ImageOps
+from string import Template
 import requests
 import math
 import threading
@@ -23,29 +24,25 @@ class GsiTile:
     zoom_level: int = 10
 
 @dataclass
-class GeoCoordRectangle:
-    lat_north: float = 0.0
-    lat_south: float = 0.0
-    lng_east:  float = 0.0
-    lng_west:  float = 0.0
-
-@dataclass
 class TimeJsonElement:
     basetime: str
     validtime: str
     elements: List[str]
 
 def geocoord2webcoord(lat: float, lng: float, zoom: int) -> [float, float]:
+    """緯度，経度をWebメルカトル座標に変換する"""
     return [
         (1 << zoom) * ( .5 - math.log(math.tan(math.pi/4 + lat*math.pi/180/2)) / (2*math.pi)),
         (1 << zoom) * (lng + 180) / 360
     ]
 
-def geocoord2tile(lat: float, lng: float, zoom: int) -> GsiTile: 
+def geocoord2tile(lat: float, lng: float, zoom: int) -> GsiTile:
+    """緯度，経度を含むWebメルカトルタイルに変換する"""
     centre_webcoord = geocoord2webcoord(lat, lng, zoom)
     return GsiTile(x=int(centre_webcoord[1]), y=int(centre_webcoord[0]), zoom_level=zoom)
 
 def geocoord2tiles(lat: float, lng: float, zoom: int, around_tiles: int) -> List[GsiTile]:
+    """緯度，経度を含むWebメルカトルタイル及びその半径aマスのタイルを取得する"""
     res = []
     centre_tile = geocoord2tile(lat=lat, lng=lng, zoom=zoom)
 
@@ -59,6 +56,7 @@ def geocoord2tiles(lat: float, lng: float, zoom: int, around_tiles: int) -> List
     return res
 
 def get_timejson() -> Optional[List[TimeJsonElement]]:
+    """有効な時刻を取得する"""
     url = 'https://www.jma.go.jp/bosai/jmatile/data/nowc/targetTimes_N1.json'
     response = requests.get(url)
     if response.status_code == 200:
@@ -66,52 +64,54 @@ def get_timejson() -> Optional[List[TimeJsonElement]]:
         return data
     return None
 
-def get_latest_jma_image(lat: float, lng: float, zoom: int, around_tiles: int) -> Image:
-    latest_time = max([i.basetime for i in get_timejson() if i.basetime==i.validtime])
+def get_tile_image(url_template: Template, lat: float, lng: float, zoom: int, around_tiles: int) -> Image:
+    """緯度，経度を含むWebメルカトルタイル及びその半径aマスのタイルを，タイルサーバから画像を取得し結合する"""
     urls = []
     for tile in geocoord2tiles(lat=lat, lng=lng, around_tiles=around_tiles, zoom=zoom):
-        urls.append(f'https://www.jma.go.jp/bosai/jmatile/data/nowc/{latest_time}/none/{latest_time}/surf/hrpns/{zoom}/{tile.x}/{tile.y}.png')
+        urls.append(
+            url_template.substitute({
+                'zoom': zoom,
+                'x': tile.x,
+                'y': tile.y
+            })
+        )
     res = []
     for url in urls:
         res.append(Image.open(BytesIO(requests.get(url).content)))
     dst_image = Image.new('RGBA', (256 * (2*around_tiles+1), 256 * (2*around_tiles+1)))
     for idx, elem in enumerate(res):
         dst_image.paste(elem, (256 * (idx // (around_tiles*2+1)), 256 * (idx % (around_tiles*2+1))))
-    dst_image.save('a.png')
     return dst_image
+
+def get_latest_jma_image(lat: float, lng: float, zoom: int, around_tiles: int) -> Image:
+    """気象庁雨雲レーダー画像を取得する"""
+    latest_time = max([i.basetime for i in get_timejson() if i.basetime==i.validtime])
+
+    return get_tile_image(
+        url_template=Template(f'https://www.jma.go.jp/bosai/jmatile/data/nowc/{latest_time}/none/{latest_time}/surf/hrpns/${{zoom}}/${{x}}/${{y}}.png'),
+        lat=lat, lng=lng, zoom=zoom, around_tiles=around_tiles
+    )
+
 
 def get_osm_image(lat: float, lng: float, zoom: int, around_tiles: int) -> Image:
-    latest_time = max([i.basetime for i in get_timejson() if i.basetime==i.validtime])
-    urls = []
-    for tile in geocoord2tiles(lat=lat, lng=lng, around_tiles=around_tiles, zoom=zoom):
-        s = random.choice(['a','b','c'])
-        urls.append(f'https://{s}.tile.openstreetmap.org/{zoom}/{tile.x}/{tile.y}.png')
-    res = []
-    for url in urls:
-        res.append(Image.open(BytesIO(requests.get(url).content)))
-    dst_image = Image.new('RGBA', (256 * (2*around_tiles+1), 256 * (2*around_tiles+1)))
-    for idx, elem in enumerate(res):
-        dst_image.paste(elem, (256 * (idx // (around_tiles*2+1)), 256 * (idx % (around_tiles*2+1))))
-    dst_image.save('b.png')
-    return dst_image
+    """OpenStreatMap画像を取得する"""
+    s = random.choice(['a','b','c'])
+    return get_tile_image(
+        url_template=Template(f'https://{s}.tile.openstreetmap.org/${{zoom}}/${{x}}/${{y}}.png'),
+        lat=lat, lng=lng, zoom=zoom, around_tiles=around_tiles
+    )
     
-def get_latest_jma_image_with_map(lat: float, lng: float, zoom: int, around_tiles: int) -> Image:
+def jma_amesh(lat: float, lng: float, zoom: int, around_tiles: int) -> Image:
+    """
+    気象庁雨雲レーダーとOpenStreatMap画像を取得して結合する
+    Usage: jma_amesh(lat=37, lng=139, zoom=8, around_tiles=2).save('res2.png')
+    """
     jma_image = get_latest_jma_image(lat=lat, lng=lng, zoom=zoom, around_tiles=around_tiles)
-    alpha255 = jma_image.copy()
-    alpha255.putalpha(0)
+    jma_image_alpha = jma_image.copy()
+    jma_image_alpha.putalpha(0)
     jma_image_mask = jma_image.copy().getchannel('A')
-    jma_image_mask.save('e.png')
-    jma_image.save('f.png')
     jma_image.putalpha(128)
-    # jma_image.composite(jma_image_mask)
-    jma_image.save('d.png')
-    jma_image_trans = Image.composite(jma_image, alpha255, jma_image_mask)
-    jma_image_trans.save('g.png')
+    jma_image_trans = Image.composite(jma_image, jma_image_alpha, jma_image_mask)
     osm_image = get_osm_image(lat=lat, lng=lng, zoom=zoom, around_tiles=around_tiles)
-    res = Image.alpha_composite(osm_image, jma_image_trans)
-    res.save('c.png')
+    return Image.alpha_composite(osm_image, jma_image_trans)
 
-print(max([i.basetime for i in get_timejson()]))
-print(get_latest_jma_image_with_map(lat=35.5, lng=139.5, zoom=9, around_tiles=1))
-print(get_osm_image(lat=35.5, lng=139.5, zoom=9, around_tiles=1))
-print(get_latest_jma_image(lat=35.5, lng=139.5, zoom=9, around_tiles=1))
