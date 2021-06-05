e42bd597d532ece57ac6a678184f872ff005f69d
m77so <m77code@gmail.com>
2021-06-05 23:41:59 +0900

    fix lint

--- tests/plugins/test_hato.py
+++ tests/plugins/test_hato.py
@@ -92,7 +92,8 @@ class TestAmesh(unittest.TestCase):
         :param msg: Slackに投稿されて欲しいメッセージ
         """
         with open(os.path.join(os.path.dirname(__file__), 'test.png'), mode='rb') as picture_file:
-            with open(os.path.join(os.path.dirname(__file__), 'test_targetTimes_N1.json'), mode='rb') as json_file:
+            with open(os.path.join(os.path.dirname(__file__), 'test_targetTimes_N1.json'), mode='rb')
+                as json_file:
                 client1 = self.get_amesh_test(mocker,
                                               place,
                                               picture_file.read(),
