a9b68729de5a25a6d8fd2de770565d39a46ddca0
massongit <suzukimasaya428@gmail.com>
2021-06-15 20:26:45 +0900

    Fix

--- run.py
+++ run.py
@@ -75,7 +75,8 @@ def on_app_mention(event_data):
             "DELETE FROM slack_client_msg_id WHERE '00:10:00' < CURRENT_TIMESTAMP - created_at",
             (client_msg_id,))
         cursor.execute(
-            'INSERT INTO slack_client_msg_id(client_msg_id,created_at) VALUES(%s, CURRENT_TIMESTAMP)',
+            'INSERT INTO slack_client_msg_id(client_msg_id, created_at) '
+            'VALUES(%s, CURRENT_TIMESTAMP)',
             (client_msg_id,))
         _db.conn.commit()
 
