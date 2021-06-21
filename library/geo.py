844fe9f9fdb69db1af45acda6b96b73f9ba11a1f
massongit <suzukimasaya428@gmail.com>
2021-06-21 23:02:01 +0900

    Fix

--- library/geo.py
+++ library/geo.py
@@ -19,7 +19,9 @@ def get_geo_data(place: str) -> Optional[Dict[str, str]]:
     :param place: 地名・住所
     :return: place: 地名, lat: 緯度, lon: 経度
     """
-    if re.match(r'[0-9]{3}-[0-9]{4}', place):
+    is_zip_code = re.match(r'[0-9]{3}-[0-9]{4}', place)
+
+    if is_zip_code:
         url = 'https://map.yahooapis.jp/search/zip/V1/zipCodeSearch'
     else:
         url = 'https://map.yahooapis.jp/geocode/V1/geoCoder'
@@ -34,12 +36,20 @@ def get_geo_data(place: str) -> Optional[Dict[str, str]]:
         geo_data = json.loads(res.content)
         if 'Feature' in geo_data:
             for feature in geo_data['Feature']:
-                if 'Name' in feature and feature['Name'] \
-                        and 'Geometry' in feature and feature['Geometry']:
+                if 'Geometry' in feature and feature['Geometry']:
                     geometry = feature['Geometry']
                     if 'Coordinates' in geometry and geometry['Coordinates']:
                         coordinates = geometry['Coordinates']
                         lon, lat = coordinates.split(',', maxsplit=2)
-                        return {'place': feature['Name'], 'lat': lat, 'lon': lon}
+                        res = {'lat': lat, 'lon': lon}
+
+                        if is_zip_code and 'Address' in feature and feature['Address']:
+                            res['place'] = feature['Address']
+                        elif not is_zip_code and 'Name' in feature and feature['Name']:
+                            res['place'] = feature['Name']
+                        else:
+                            return None
+
+                        return res
 
     return None
