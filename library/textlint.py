a0a91be30abab3f83de7a67ef458e60fd7253c42
massongit <suzukimasaya428@gmail.com>
2021-06-23 23:12:44 +0900

    Fix

--- library/textlint.py
+++ library/textlint.py
@@ -11,26 +11,27 @@ from typing import Optional
 
 def get_textlint_result(text: str) -> Optional[str]:
     """textlintを使って文章校正を行う"""
-    process = subprocess.run(['node_modules/.bin/textlint', '--stdin'],
-                             input=text, encoding='UTF-8', check=True,
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    try:
+        process = subprocess.run(['node_modules/.bin/textlint', '--stdin'],
+                                 input=text, encoding='UTF-8', check=True,
+                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError:
+        if process.stderr is not None:
+            return process.stderr
 
-    if process.stderr is not None:
-        return process.stderr
+        is_start_output = False
+        res = list()
 
-    is_start_output = False
-    res = list()
+        for line in process.stdout.split(os.linesep):
+            line = line.strip()
+            if is_start_output:
+                res.append(line)
+            elif line == '<text>':
+                is_start_output = True
 
-    for line in process.stdout.split(os.linesep):
-        line = line.strip()
-        if is_start_output:
-            res.append(line)
-        elif line == '<text>':
-            is_start_output = True
+        res = os.linesep.join(res).strip()
 
-    res = os.linesep.join(res).strip()
-
-    if res:
-        return '```' + res + '```'
+        if res:
+            return '```' + res + '```'
 
     return None
