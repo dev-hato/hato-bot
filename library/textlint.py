2b86b82dc4d6ddf1d8ddc64558fe73e8b24abeb5
massongit <suzukimasaya428@gmail.com>
2021-06-23 23:16:06 +0900

    Fix

--- library/textlint.py
+++ library/textlint.py
@@ -11,8 +11,9 @@ from typing import Optional
 
 def get_textlint_result(text: str) -> Optional[str]:
     """textlintを使って文章校正を行う"""
+    # pylint: disable=W1510
     process = subprocess.run(['node_modules/.bin/textlint', '--stdin'],
-                             input=text, encoding='UTF-8', check=True,
+                             input=text, encoding='UTF-8',
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     if process.stderr is not None:
