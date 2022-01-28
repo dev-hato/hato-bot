1bc3af87c3ac981402afebf5ff1ede1d3739b522
masaya.suzuki <suzukimasaya428@gmail.com>
2022-01-28 09:41:55 +0900

    SQLの予約語を大文字にする

--- setup/pgsql-init/02_init.sql
+++ setup/pgsql-init/02_init.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS vocabulary
 (
-    "no" serial UNIQUE,
-    word text
+    "no" SERIAL UNIQUE,
+    word TEXT
 );
 CREATE TABLE IF NOT EXISTS slack_client_msg_id
 (
-    client_msg_id text UNIQUE,
+    client_msg_id TEXT UNIQUE,
     created_at TIMESTAMP
 );
