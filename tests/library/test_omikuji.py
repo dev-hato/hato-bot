ae879e159e04ff8ea0606b813221e07028f28a30
fono09 <fono@fono.jp>
2022-03-06 00:48:30 +0900

    おみくじライブラリを書いた

---
+++ tests/library/test_omikuji.py
@@ -0,0 +1,38 @@
+"""
+omikujiライブラリのテスト
+"""
+
+import unittest
+from library.omikuji import AbstractOmikujiResults, OmikujiResult, Omikuji, test
+    
+
+class TestOmikuji(unittest.TestCase):
+    """
+    おみくじのテスト
+    """
+
+    def test_omikuji_config:
+        """
+        おみくじの設定が正常か
+        """
+        test()
+
+
+    def test_omikuji_minimum:
+        """
+        おみくじの実装が正常か
+        """
+
+        class TestOmikujiResults(AbstractOmikujiResults):
+            Kichi = auto()
+            SueKichi = auto()
+
+        testOmikuji = Omikuji(entries={
+            TestOmikujiResults.Kichi: OmikujiResult(TestOmikujiResults.Kichi, 0.5, "吉"),
+            TestOmikujiResults.SueKichi: OmikujiResult(TestOmikujiResults.SueKichi, 0.5, "末吉"),
+        })
+
+        self.assertTrue(
+            testOmikuji.draw().message == "吉"
+            or testOmikuji.draw().message == "末吉"
+        )
