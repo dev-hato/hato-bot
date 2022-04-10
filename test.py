7d3a53fcfc99daf9d4c60ee428096eaa81f4f9b1
massongit <suzukimasaya428@gmail.com>
2022-04-10 17:07:42 +0900

    pylintに従ってみる

--- test.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-a = subprocess.run(
-    ["yarn", "run", "textlint", "--stdin"],
-    input="我々は幽霊です。\nそうだ。",
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-)
-print(a.stderr)
