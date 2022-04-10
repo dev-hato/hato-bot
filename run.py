29a46a1c836f36b3ac3d0f36028d28334e6ee22a
massongit <suzukimasaya428@gmail.com>
2022-04-10 17:37:07 +0900

    再び反映してみる

--- run.py
+++ run.py
@@ -3,41 +3,45 @@
 """
 BotのMain関数
 """
-import sys
 import logging
 import logging.config
-from typing import Callable, List
+import sys
 from concurrent.futures import ThreadPoolExecutor
+from typing import Callable, List
+
+from flask import Flask, escape, jsonify, request
 from slackeventsapi import SlackEventAdapter
-from flask import Flask, request, escape, jsonify
+
 import slackbot_settings as conf
-from plugins import analyze
-from library.clientclass import SlackClient, ApiClient
+from library.clientclass import ApiClient, SlackClient
 from library.database import Database
+from plugins import analyze
 
 app = Flask(__name__)
 
 
 slack_events_adapter = SlackEventAdapter(
-    signing_secret=conf.SLACK_SIGNING_SECRET, endpoint="/slack/events", server=app)
+    signing_secret=conf.SLACK_SIGNING_SECRET, endpoint="/slack/events", server=app
+)
 
 
 def __init__():
     log_format_config = {
-        'format': '[%(asctime)s] %(message)s',
-        'datefmt': '%Y-%m-%d %H:%M:%S',
-        'level': logging.DEBUG,
-        'stream': sys.stdout,
+        "format": "[%(asctime)s] %(message)s",
+        "datefmt": "%Y-%m-%d %H:%M:%S",
+        "level": logging.DEBUG,
+        "stream": sys.stdout,
     }
     logging.basicConfig(**log_format_config)
-    logging.getLogger('requests.packages.urllib3.connectionpool').setLevel(
-        logging.WARNING)
+    logging.getLogger("requests.packages.urllib3.connectionpool").setLevel(
+        logging.WARNING
+    )
 
 
 def analyze_slack_message(messages: List[dict]) -> Callable[[SlackClient], None]:
     """Slackコマンド解析"""
 
-    message = ''.join([m['text'] for m in messages if 'text' in m]).strip()
+    message = "".join([m["text"] for m in messages if "text" in m]).strip()
     return analyze.analyze_message(message)
 
 
@@ -48,48 +52,56 @@ def on_app_mention(event_data):
     """
 
     channel = event_data["event"]["channel"]
-    blocks = event_data['event']['blocks']
-    authed_users = event_data['authed_users']
-    client_msg_id = event_data['event']['client_msg_id']
+    blocks = event_data["event"]["blocks"]
+    authed_users = event_data["authed_users"]
+    client_msg_id = event_data["event"]["client_msg_id"]
 
-    print(f'event_data: {event_data}')
-    print(f'channel: {channel}')
-    print(f'blocks: {blocks}')
-    print(f'authed_users: {authed_users}')
-    print(f'client_msg_id: {client_msg_id}')
+    print(f"event_data: {event_data}")
+    print(f"channel: {channel}")
+    print(f"blocks: {blocks}")
+    print(f"authed_users: {authed_users}")
+    print(f"client_msg_id: {client_msg_id}")
 
     with Database() as _db, _db.conn.cursor() as cursor:
         cursor.execute(
-            'SELECT client_msg_id FROM slack_client_msg_id WHERE client_msg_id = %s LIMIT 1',
-            (client_msg_id,))
+            "SELECT client_msg_id FROM slack_client_msg_id WHERE client_msg_id = %s LIMIT 1",
+            (client_msg_id,),
+        )
 
         if cursor.fetchone():
-            print('skip')
+            print("skip")
             return
 
         cursor.execute(
             "DELETE FROM slack_client_msg_id "
             "WHERE created_at < CURRENT_TIMESTAMP - interval '10 minutes'",
-            (client_msg_id,))
+            (client_msg_id,),
+        )
         cursor.execute(
-            'INSERT INTO slack_client_msg_id(client_msg_id, created_at) '
-            'VALUES(%s, CURRENT_TIMESTAMP)',
-            (client_msg_id,))
+            "INSERT INTO slack_client_msg_id(client_msg_id, created_at) "
+            "VALUES(%s, CURRENT_TIMESTAMP)",
+            (client_msg_id,),
+        )
         _db.conn.commit()
 
     with ThreadPoolExecutor(max_workers=3) as tpe:
         for block in blocks:
-            if block['type'] == 'rich_text':
-                block_elements = block['elements']
+            if block["type"] == "rich_text":
+                block_elements = block["elements"]
                 for block_element in block_elements:
-                    if block_element['type'] == 'rich_text_section':
-                        block_element_elements = block_element['elements']
-                        if len(block_element_elements) > 0 and \
-                                block_element_elements[0]['type'] == 'user' and \
-                                block_element_elements[0]['user_id'] in authed_users:
-                            tpe.submit(analyze_slack_message(block_element_elements[1:]),
-                                       SlackClient(channel,
-                                                   block_element_elements[0]['user_id']))
+                    if block_element["type"] == "rich_text_section":
+                        block_element_elements = block_element["elements"]
+                        if (
+                            len(block_element_elements) > 0
+                            and block_element_elements[0]["type"] == "user"
+                            and block_element_elements[0]["user_id"] in authed_users
+                        ):
+                            tpe.submit(
+                                analyze_slack_message(block_element_elements[1:]),
+                                SlackClient(
+                                    channel, block_element_elements[0]["user_id"]
+                                ),
+                            )
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -105,11 +117,11 @@ def http_app():
 
     pipenv run python post_command.py --channel C0123A4B5C6 --user U012A34BCDE "鳩"
     """
-    msg = request.json['message']
-    channel = request.json['channel']
-    user = request.json['user']
+    msg = request.json["message"]
+    channel = request.json["channel"]
+    user = request.json["user"]
     client = SlackClient(channel, user)
-    client.post(f'コマンド: {msg}')
+    client.post(f"コマンド: {msg}")
     analyze.analyze_message(msg)(client)
     return "success"
 
@@ -124,25 +136,25 @@ def healthcheck_app():
     curl -XPOST -d '{"message": "鳩"}' \
         -H "Content-Type: application/json" http://localhost:3000/healthcheck
     """
-    msg = escape(request.json['message'])
+    msg = escape(request.json["message"])
     client = ApiClient()
-    client.post(f'コマンド: {msg}')
+    client.post(f"コマンド: {msg}")
     analyze.analyze_message(msg)(client)
     return client.response
 
 
-@app.route('/status', methods=["GET"])
+@app.route("/status", methods=["GET"])
 def status():
     """
     死活監視のためのレスポンスをJSON形式で返します
     """
-    return jsonify({'message': 'hato-bot is running', 'version': conf.VERSION}), 200
+    return jsonify({"message": "hato-bot is running", "version": conf.VERSION}), 200
 
 
 def main():
     """メイン関数"""
 
-    app.run(host='0.0.0.0', port=conf.PORT)
+    app.run(host="0.0.0.0", port=conf.PORT)
 
 
 if __name__ == "__main__":
