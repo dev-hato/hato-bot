c442fd77aafe613b8ea32f4f4de79399a48d5bce
masaya.suzuki <suzukimasaya428@gmail.com>
2022-04-10 20:50:39 +0900

    super-linter向けテストケース追加

---
+++ test_/test_black.py
@@ -0,0 +1,2 @@
+m = [1, 2, 3]
+print(m[len("test") :])
---
+++ test_/test_pylint.py
@@ -0,0 +1,21 @@
+from flask import Flask, escape, jsonify, request
+from git import Repo
+from git.exc import GitCommandNotFound, InvalidGitRepositoryError
+from slack import WebClient
+from slackeventsapi import SlackEventAdapter
+
+import settings as conf
+
+WebClient()
+Flask(__name__)
+escape('test')
+jsonify({'foo': 'var'})
+SlackEventAdapter(signing_secret='test')
+
+print(conf.TEST)
+print(request.json['test'])
+
+try:
+    repo = Repo()
+except (InvalidGitRepositoryError, GitCommandNotFound):
+    pass
