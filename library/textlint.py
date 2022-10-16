753e11be301aef41fea5fe62616a0943ef28454d
masaya.suzuki <suzukimasaya428@gmail.com>
2022-10-16 20:49:57 +0900

    処理修正

--- library/textlint.py
+++ library/textlint.py
@@ -4,7 +4,6 @@
 textlintを使って文章校正を行う
 """
 
-import os
 import subprocess
 from typing import Optional
 
@@ -19,23 +18,7 @@ def get_textlint_result(text: str) -> Optional[str]:
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
     )
-
-    if process.stderr is not None:
-        return process.stderr
-
-    is_start_output = False
-    res = list()
-
-    for line in process.stdout.split(os.linesep):
-        line = line.strip()
-        if is_start_output:
-            res.append(line)
-        elif line == "<text>":
-            is_start_output = True
-
-    res = os.linesep.join(res).strip()
-
-    if res:
-        return "```" + res + "```"
-
-    return None
+    for fd in [process.stderr, process.stdout]:
+        res = fd.strip()
+        if res:
+            return "```" + res + "```"
