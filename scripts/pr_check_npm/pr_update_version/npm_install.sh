164bd9c67fc1e23f734c79b886802075ed9d3c0e
masaya.suzuki <suzukimasaya428@gmail.com>
2022-11-03 21:32:39 +0900

    rm script

--- scripts/pr_check_npm/pr_update_version/npm_install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-DEPENDABOT_NPM_VERSION="${{steps.get_dependabot_npm_version.outputs.npm_version}}"
-NPM_PATTERN_PACKAGE="s/\"npm\": \".*\"/\"npm\": \"^${DEPENDABOT_NPM_VERSION}\"/g"
-sed -i -e "${NPM_PATTERN_PACKAGE}" package.json
