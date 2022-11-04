a587b3e35b1d0e726373b37460ec6bde66292f5b
masaya.suzuki <suzukimasaya428@gmail.com>
2022-11-05 02:57:50 +0900

    test

--- scripts/pr_copy_ci_hato_bot/pr_copy_ci/copy_ci.sh
+++ scripts/pr_copy_ci_hato_bot/pr_copy_ci/copy_ci.sh
@@ -1,24 +1,34 @@
 #!/usr/bin/env bash
 
 workflows_path=.github/workflows
+echo A
 find "${GITHUB_WORKSPACE}/sudden-death/${workflows_path}" -type f \
   -not -name "*sudden-death.yml" \
   -exec rm -f {} \;
-
+echo B
 for f in $(find "${GITHUB_WORKSPACE}/hato-bot/${workflows_path}" -type f \
   -not -name "*hato-bot.yml" | sed -e "s:hato-bot/${workflows_path}/::g"); do
+    echo yq '(.jobs.*.steps.[] | select(has("with")).with | select(has("repo-name")).repo-name) = "dev-hato/sudden-death"' "${GITHUB_WORKSPACE}/hato-bot/${workflows_path}/${f}" ">${GITHUB_WORKSPACE}/sudden-death/${workflows_path}/${f}"
   yq '(.jobs.*.steps.[] | select(has("with")).with | select(has("repo-name")).repo-name) = "dev-hato/sudden-death"' "${GITHUB_WORKSPACE}/hato-bot/${workflows_path}/${f}" >"${GITHUB_WORKSPACE}/sudden-death/${workflows_path}/${f}"
 done
-
+echo C
 for f in $(find "${GITHUB_WORKSPACE}/hato-bot/scripts" -type f | grep -v hato_bot | sed -e "s:hato-bot/::g"); do
+  echo mkdir -p "${GITHUB_WORKSPACE}/sudden-death/$(dirname "${f}")"
   mkdir -p "${GITHUB_WORKSPACE}/sudden-death/$(dirname "${f}")"
+  echo cp "${GITHUB_WORKSPACE}/hato-bot/${f}" "${GITHUB_WORKSPACE}/sudden-death/${f}"
   cp "${GITHUB_WORKSPACE}/hato-bot/${f}" "${GITHUB_WORKSPACE}/sudden-death/${f}"
 done
-
+echo D
 for f in .markdown-lint.yml .python-lint .textlintrc .gitleaks.toml .mypy.ini .pre-commit-config.yaml .python-version .pep8 .flake8 .python-black .isort.cfg renovate.json; do
+  echo cp "${GITHUB_WORKSPACE}/hato-bot/${f}" "${GITHUB_WORKSPACE}/sudden-death/"
   cp "${GITHUB_WORKSPACE}/hato-bot/${f}" "${GITHUB_WORKSPACE}/sudden-death/"
 done
+echo E
 PATTERN_BEFORE="$(grep '^click' "${GITHUB_WORKSPACE}/sudden-death/Pipfile")"
+echo F
 PATTERN_AFTER="$(grep '^click' "${GITHUB_WORKSPACE}/hato-bot/Pipfile")"
+echo G
 PATTERN="s/${PATTERN_BEFORE}/${PATTERN_AFTER}/g"
+echo H
 sed -i -e "${PATTERN}" "${GITHUB_WORKSPACE}/sudden-death/Pipfile"
+echo I
