233a940df12b9dc21798cf742ae3cd1a282c6ad9
masaya.suzuki <suzukimasaya428@gmail.com>
2022-11-15 23:35:04 +0900

    CIのスクリプトをまとめる

--- scripts/deploy_hato_bot/dockle/run_dockle.sh
+++ scripts/deploy_hato_bot/dockle/run_dockle.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+cp .env.example .env
+TAG_NAME="${HEAD_REF//\//-}"
+dockle_version="$(cat .dockle-version)"
+curl -L -o dockle.deb "https://github.com/goodwithtech/dockle/releases/download/v${dockle_version}/dockle_${dockle_version}_Linux-64bit.deb"
+sudo dpkg -i dockle.deb
+docker compose pull
+docker compose up -d
+
 for image_name in $(docker compose images | awk 'OFS=":" {print $2,$3}' | tail -n +2); do
   cmd="dockle --exit-code 1 "
 
--- scripts/pr_test/pr_super_lint/set_venv_path.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# 環境ファイルを使ってenvにsetしている
-# 参考URL: https://bit.ly/2KJhjqk
-venv_path=$(pipenv --venv)
-echo "${venv_path}"
-echo "venv_path=${venv_path}" >>"${GITHUB_ENV}"
