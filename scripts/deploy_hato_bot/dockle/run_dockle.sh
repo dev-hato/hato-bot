619d23645935bdab0ce1199cb45ae354f4973e84
masaya.suzuki <suzukimasaya428@gmail.com>
2022-11-16 00:00:30 +0900

    スクリプト修正

--- scripts/deploy_hato_bot/dockle/run_dockle.sh
+++ scripts/deploy_hato_bot/dockle/run_dockle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cp .env.example .env
-TAG_NAME="${HEAD_REF//\//-}"
+export TAG_NAME="${HEAD_REF//\//-}"
 dockle_version="$(cat .dockle-version)"
 curl -L -o dockle.deb "https://github.com/goodwithtech/dockle/releases/download/v${dockle_version}/dockle_${dockle_version}_Linux-64bit.deb"
 sudo dpkg -i dockle.deb
--- scripts/deploy_hato_bot/update_version_python_version/get_python_version.sh
+++ scripts/deploy_hato_bot/update_version_python_version/get_python_version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cp .env.example .env
-TAG_NAME="${HEAD_REF//\//-}"
+export TAG_NAME="${HEAD_REF//\//-}"
 docker compose pull
 DOCKER_CMD="python --version 2>&1 | sed -e 's/^Python //g'"
 python_version=$(docker compose run hato-bot sh -c "${DOCKER_CMD}")
