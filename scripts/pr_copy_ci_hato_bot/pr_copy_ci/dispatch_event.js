5acb58fb14cdb8a83f4a7f9824c751ea84bf0cd2
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2023-08-25 20:57:21 +0900

    ref修正

--- scripts/pr_copy_ci_hato_bot/pr_copy_ci/dispatch_event.js
+++ scripts/pr_copy_ci_hato_bot/pr_copy_ci/dispatch_event.js
@@ -3,7 +3,7 @@ module.exports = async ({ github, context }) => {
     owner: context.repo.owner,
     repo: 'sudden-death',
     workflow_id: 2452313,
-    ref: 'main'
+    ref: 'master'
   }
   console.log('call actions.createWorkflowDispatch:')
   console.log(actionsCreateWorkflowDispatchParams)
