56987ebbf0362d592093691a9b4089615c5ae2a6
massongit <suzukimasaya428@gmail.com>
2023-09-23 22:50:00 +0900

    super-linterに与えるPATHを元々のPATHを元に指定する

--- scripts/pr_test/pr_super_lint/npm_ci.sh
+++ scripts/pr_test/pr_super_lint/npm_ci.sh
@@ -2,3 +2,6 @@
 
 bash "${GITHUB_WORKSPACE}/scripts/npm_ci.sh"
 echo "PYTHONPATH=/github/workspace/:/github/workflow/.venv/lib/python$(echo 'import sys; print(".".join(map(str, sys.version_info[0:2])))' | python)/site-packages" >> "${GITHUB_ENV}"
+action="$(yq '.jobs.super-linter.steps[-1].uses' .github/workflows/super-linter.yml)"
+PATH="$(docker run --rm --entrypoint '' "ghcr.io/${action//\/slim@/:slim-}" /bin/sh -c 'echo $PATH')"
+echo "PATH=/github/workspace/node_modules/.bin:${PATH}" >> "$GITHUB_ENV"
