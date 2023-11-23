42121f4a6c4dae992fd0879ad0da7b392605f151
massongit <suzukimasaya428@gmail.com>
2023-11-23 11:56:17 +0900

    型修正

--- library/chat_gpt.py
+++ library/chat_gpt.py
@@ -1,11 +1,12 @@
 from openai import OpenAI
+from typing import Optional
 
 import slackbot_settings as conf
 
 client = OpenAI(api_key=conf.OPENAI_API_KEY)
 
 
-def chat_gpt(message: str) -> str:
+def chat_gpt(message: str) -> Optional[str]:
     result = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
@@ -16,6 +17,6 @@ def chat_gpt(message: str) -> str:
     return result.choices[0].message.content
 
 
-def image_create(message: str) -> str:
+def image_create(message: str) -> Optional[str]:
     response = client.images.generate(prompt=message, n=1, size="512x512")
     return response.data[0].url
