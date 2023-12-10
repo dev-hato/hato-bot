8881c3ca67898f851fb35672b8f5223e30a17006
massongit <suzukimasaya428@gmail.com>
2023-12-10 19:18:20 +0900

    Misskey: UTF-8絵文字を変換して投稿する

--- library/clientclass.py
+++ library/clientclass.py
@@ -8,7 +8,7 @@ from abc import ABCMeta, abstractmethod
 
 import discord
 from slack_sdk import WebClient
-
+import emoji
 
 class BaseClient(metaclass=ABCMeta):
     """
@@ -162,7 +162,7 @@ class MisskeyClient(BaseClient):
 
     def _post(self, text=None, file_ids=None):
         self.client.notes_create(
-            text=text, reply_id=self.message["id"], file_ids=file_ids
+            text=emoji.emojize(text, language='alias'), reply_id=self.message["id"], file_ids=file_ids
         )
 
     def get_send_user(self):
