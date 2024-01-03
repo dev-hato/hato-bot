d50869276e1a77cbac126df8e85a71fcbc5a8006
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2024-01-03 13:12:53 +0900

    Add echo

--- scripts/pr_format/pr_format/format.sh
+++ scripts/pr_format/pr_format/format.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+echo aaa
 pipenv run python "${GITHUB_WORKSPACE}/scripts/pr_format/pr_format/fix_pipfile.py"
+echo iii
 pipenv install --dev
+echo uuu
 pipenv run autopep8 --exit-code --in-place --recursive .
+echo eee
 pipenv run black --config .python-black .
+echo ooo
 pipenv run isort --sp .isort.cfg .
+echo kakaka
