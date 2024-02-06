918545c7a9c0147524b5281f8c25b14f718e7f04
massongit <suzukimasaya428@gmail.com>
2024-02-06 09:31:13 +0900

    super-linterによる指摘事項修正

--- scripts/pr_test/pr_super_lint/install_pipenv.sh
+++ scripts/pr_test/pr_super_lint/install_pipenv.sh
@@ -4,9 +4,9 @@ file_name=Dockerfile
 package_name=pipenv
 
 if [ -f ${file_name} ]; then
-  package_name_v=$(grep -oE "${package_name}[^ ]+" ${file_name})
+	package_name_v=$(grep -oE "${package_name}[^ ]+" ${file_name})
 else
-  package_name_v=${package_name}
+	package_name_v=${package_name}
 fi
 
 pip install "${package_name_v}"
