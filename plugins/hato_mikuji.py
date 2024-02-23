b94bcd6f73b34f69dfe56d9deca89eb19f3c1f6d
fono <fono@pixiv.co.jp>
2024-02-23 21:22:38 +0900

    hato-bot用おみくじセットアップをplugins/hato_mikujiに分離

---
+++ plugins/hato_mikuji.py
@@ -0,0 +1,52 @@
+"""
+hato-bot用のおみくじラッパー
+"""
+
+from enum import Enum, auto
+from library.omikuji import OmikujiResult, OmikujiResults, draw
+
+# 以下おみくじの設定
+class OmikujiEnum(Enum):
+    """
+    おみくじの結果一覧
+    """
+    DAI_KICHI = auto()
+    CHU_KICHI = auto()
+    SHO_KICHI = auto()
+    KICHI = auto()
+    SUE_KICHI = auto()
+    AGE_KICHI = auto()
+    KYO = auto()
+    DAI_KYO = auto()
+
+class HatoMikuji:
+    OMIKUJI_CONFIG = OmikujiResults(
+        {
+            OmikujiEnum.DAI_KICHI: OmikujiResult(
+                12, ":tada: 大吉 何でもうまくいく!!気がする!!"
+            ),
+            OmikujiEnum.KICHI: OmikujiResult(100, ":smirk: 吉 まあうまくいくかも!?"),
+            OmikujiEnum.CHU_KICHI: OmikujiResult(
+                100, ":smile: 中吉 そこそこうまくいくかも!?"
+            ),
+            OmikujiEnum.SHO_KICHI: OmikujiResult(
+                100, ":smiley: 小吉 なんとなくうまくいくかも!?"
+            ),
+            OmikujiEnum.SUE_KICHI: OmikujiResult(
+                37, ":expressionless: 末吉 まあ多分うまくいくかもね……!?"
+            ),
+            OmikujiEnum.AGE_KICHI: OmikujiResult(
+                2, ":poultry_leg: 揚げ吉 鳩を揚げると良いことあるよ!!"
+            ),
+            OmikujiEnum.KYO: OmikujiResult(
+                12, ":cry: 凶 ちょっと慎重にいったほうがいいかも……"
+            ),
+            OmikujiEnum.DAI_KYO: OmikujiResult(
+                2, ":crying_cat_face: 大凶 そういう時もあります……猫になって耐えましょう"
+            ),
+        }
+    )
+
+    @classmethod
+    def draw(cls):
+        return draw(cls.OMIKUJI_CONFIG)[1].message
