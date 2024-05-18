c973418d12403d717f1160d5a8bbf558c1f0d6b7
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-05-19 02:14:58 +0900

    DBの設定を可能な限りfly.tomlから取得する

--- flyio_deploy.sh
+++ flyio_deploy.sh
@@ -7,7 +7,8 @@ grep -v DATABASE_URL .env | fly secrets import
 
 set -e
 POSTGRES_APP_NAME="$(yq '.app' fly.toml)-db"
-fly postgres create --name="$POSTGRES_APP_NAME" --region=nrt --initial-cluster-size=1 --vm-size=shared-cpu-1x --volume-size=1
+fly postgres create --name="$POSTGRES_APP_NAME" --region="$(yq '.primary_region' fly.toml)" --initial-cluster-size=1 \
+                    --vm-size="$(yq '.vm.[0].size' fly.toml)" --volume-size=1
 fly postgres attach "$POSTGRES_APP_NAME"
 
 # shellcheck disable=SC2046
