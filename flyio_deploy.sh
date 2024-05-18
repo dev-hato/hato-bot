b0b63ff5b2659d06cbf50e78191453e11166a524
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-05-19 02:15:32 +0900

    Secretsのセットを移動し、エラーにならないようsleep追加

--- flyio_deploy.sh
+++ flyio_deploy.sh
@@ -3,13 +3,15 @@ set -x
 
 # この時点では必要な環境変数がセットされていないため、起動に失敗する (コマンドが異常終了する)
 fly launch --copy-config --vm-memory=256mb -y
-grep -v DATABASE_URL .env | fly secrets import
 
 set -e
 POSTGRES_APP_NAME="$(yq '.app' fly.toml)-db"
 fly postgres create --name="$POSTGRES_APP_NAME" --region="$(yq '.primary_region' fly.toml)" --initial-cluster-size=1 \
                     --vm-size="$(yq '.vm.[0].size' fly.toml)" --volume-size=1
 fly postgres attach "$POSTGRES_APP_NAME"
+sleep 60
+grep -v DATABASE_URL .env | fly secrets import
+sleep 60
 
 # shellcheck disable=SC2046
 fly machine start $(fly machine list --json | yq '.[].id' | tr "\n" ' ')
