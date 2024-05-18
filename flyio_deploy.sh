0e941d2229348fa56b15c12804343e53a9ed9367
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-05-19 02:22:20 +0900

    super-linterによる指摘事項修正

--- flyio_deploy.sh
+++ flyio_deploy.sh
@@ -7,7 +7,7 @@ fly launch --copy-config --vm-memory=256mb -y
 set -e
 POSTGRES_APP_NAME="$(yq '.app' fly.toml)-db"
 fly postgres create --name="$POSTGRES_APP_NAME" --region="$(yq '.primary_region' fly.toml)" --initial-cluster-size=1 \
-                    --vm-size="$(yq '.vm.[0].size' fly.toml)" --volume-size=1
+	--vm-size="$(yq '.vm.[0].size' fly.toml)" --volume-size=1
 fly postgres attach "$POSTGRES_APP_NAME"
 sleep 60
 grep -v DATABASE_URL .env | fly secrets import
