17604ce7c4a452fee2f5da8daf2e9c177d7b88f6
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-05-19 13:00:07 +0900

    fly.io: 空のアプリに環境変数をセット後デプロイする

--- flyio_deploy.sh
+++ flyio_deploy.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
-set -x
+set -xe -o pipefail
 
-# この時点では必要な環境変数がセットされていないため、起動に失敗する (コマンドが異常終了する)
-fly launch --copy-config --vm-memory=256mb -y
-
-set -e
-POSTGRES_APP_NAME="$(yq '.app' fly.toml)-db"
+APP_NAME=$(yq '.app' fly.toml)
+fly apps create --name="$APP_NAME"
+grep -v DATABASE_URL .env | fly secrets import
+POSTGRES_APP_NAME="${APP_NAME}-db"
 fly postgres create --name="$POSTGRES_APP_NAME" --region="$(yq '.primary_region' fly.toml)" --initial-cluster-size=1 \
 	--vm-size="$(yq '.vm.[0].size' fly.toml)" --volume-size=1
 fly postgres attach "$POSTGRES_APP_NAME"
-sleep 60
-grep -v DATABASE_URL .env | fly secrets import
-sleep 60
-
-# shellcheck disable=SC2046
-fly machine start $(fly machine list --json | yq '.[].id' | tr "\n" ' ')
+fly deploy --vm-memory=256mb
