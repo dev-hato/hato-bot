fdc47a3b6091c2784b94a30b9a9ab831c937ba7b
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-08-07 08:06:48 +0900

    super-linterによる指摘事項修正

--- scripts/pr_release_hato_bot/pr_release/create_pull_request.js
+++ scripts/pr_release_hato_bot/pr_release/create_pull_request.js
@@ -1,17 +1,17 @@
-const script = require(`${process.env.GITHUB_WORKSPACE}/scripts/create_pull_request_hato_bot.js`)
+const script = require(`${process.env.GITHUB_WORKSPACE}/scripts/create_pull_request_hato_bot.js`);
 
 module.exports = async ({ github, context }) => {
   const commonParams = {
     owner: context.repo.owner,
-    repo: context.repo.repo
-  }
+    repo: context.repo.repo,
+  };
   const pullsCreateParams = {
-    head: context.repo.owner + ':develop',
-    base: 'master',
-    title: 'リリース',
-    body: '鳩は唐揚げになるため、片栗粉へ飛び込む',
+    head: context.repo.owner + ":develop",
+    base: "master",
+    title: "リリース",
+    body: "鳩は唐揚げになるため、片栗粉へ飛び込む",
     draft: true,
-    ...commonParams
-  }
-  script({ github, pullsCreateParams, commonParams })
-}
+    ...commonParams,
+  };
+  script({ github, pullsCreateParams, commonParams });
+};
