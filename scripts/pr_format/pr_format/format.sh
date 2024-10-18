d376251f077197f24cc203529c040455cd67f1ac
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-10-18 23:05:47 +0900

    pyinkのバージョンをsuper-linter準拠にする

--- scripts/pr_format/pr_format/format.sh
+++ scripts/pr_format/pr_format/format.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 pipenv run python "${GITHUB_WORKSPACE}/scripts/pr_format/pr_format/fix_pipfile.py"
+tag_name="$(yq '.jobs.pr-super-lint.steps[-1].uses' .github/workflows/pr-test.yml | sed -e 's;/slim@.*;:slim;g')"
+tag_version="$(yq '.jobs.pr-super-lint.steps[-1].uses | line_comment' .github/workflows/pr-test.yml)"
+pyink_version=$(docker run --rm --entrypoint '' "ghcr.io/${tag_name}-${tag_version}" /bin/sh -c 'pyink --version')
+sed -i -e "s/pyink = .*/pyink = \"==${pyink_version}\"/g" Pipfile
 pipenv install --dev
 pipenv run autopep8 --exit-code --in-place --recursive .
 pipenv run pyink --config .python-black .
