5cb363ea8ac1d642d3ec999dc225b538414181a4
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-10-18 23:10:06 +0900

    コマンド修正

--- scripts/pr_format/pr_format/format.sh
+++ scripts/pr_format/pr_format/format.sh
@@ -3,7 +3,7 @@
 pipenv run python "${GITHUB_WORKSPACE}/scripts/pr_format/pr_format/fix_pipfile.py"
 tag_name="$(yq '.jobs.pr-super-lint.steps[-1].uses' .github/workflows/pr-test.yml | sed -e 's;/slim@.*;:slim;g')"
 tag_version="$(yq '.jobs.pr-super-lint.steps[-1].uses | line_comment' .github/workflows/pr-test.yml)"
-pyink_version=$(docker run --rm --entrypoint '' "ghcr.io/${tag_name}-${tag_version}" /bin/sh -c 'pyink --version')
+pyink_version="$(docker run --rm --entrypoint '' "ghcr.io/${tag_name}-${tag_version}" /bin/sh -c 'pyink --version' | grep pyink | awk '{ print $2 }')"
 sed -i -e "s/pyink = .*/pyink = \"==${pyink_version}\"/g" Pipfile
 pipenv install --dev
 pipenv run autopep8 --exit-code --in-place --recursive .
