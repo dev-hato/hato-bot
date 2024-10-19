c8549872fc4cbe3b29f108d812418982629c507c
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-10-19 09:45:39 +0900

    revert

--- scripts/pr_test/pr_super_lint/npm_ci.sh
+++ scripts/pr_test/pr_super_lint/npm_ci.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 bash "${GITHUB_WORKSPACE}/scripts/npm_ci.sh"
+npm install --save-dev tsx
 echo "PYTHONPATH=/github/workspace/:/github/workflow/.venv/lib/python$(echo 'import sys; print(".".join(map(str, sys.version_info[0:2])))' | python)/site-packages" >>"${GITHUB_ENV}"
 action="$(yq '.jobs.pr-super-lint.steps[-1].uses | line_comment' .github/workflows/pr-test.yml)"
 PATH="$(docker run --rm --entrypoint '' "ghcr.io/super-linter/super-linter:slim-${action}" /bin/sh -c 'echo $PATH')"
 echo "PATH=/github/workspace/node_modules/.bin:${PATH}" >>"$GITHUB_ENV"
-npm install --save-dev tsx
