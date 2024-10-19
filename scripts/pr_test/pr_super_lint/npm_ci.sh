bf0c88a4fd8953b103bbff8fa56556fb6e757162
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-10-19 12:55:32 +0900

    fix

--- scripts/pr_test/pr_super_lint/npm_ci.sh
+++ scripts/pr_test/pr_super_lint/npm_ci.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 bash "${GITHUB_WORKSPACE}/scripts/npm_ci.sh"
-npm install --save-dev tsx
 echo "PYTHONPATH=/github/workspace/:/github/workflow/.venv/lib/python$(echo 'import sys; print(".".join(map(str, sys.version_info[0:2])))' | python)/site-packages" >>"${GITHUB_ENV}"
