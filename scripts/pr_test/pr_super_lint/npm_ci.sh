1e4ed4576d0cc9965967f3d6f5d06727c1b2aaee
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-10-19 09:37:08 +0900

    test

--- scripts/pr_test/pr_super_lint/npm_ci.sh
+++ scripts/pr_test/pr_super_lint/npm_ci.sh
@@ -5,3 +5,4 @@ echo "PYTHONPATH=/github/workspace/:/github/workflow/.venv/lib/python$(echo 'imp
 action="$(yq '.jobs.pr-super-lint.steps[-1].uses | line_comment' .github/workflows/pr-test.yml)"
 PATH="$(docker run --rm --entrypoint '' "ghcr.io/super-linter/super-linter:slim-${action}" /bin/sh -c 'echo $PATH')"
 echo "PATH=/github/workspace/node_modules/.bin:${PATH}" >>"$GITHUB_ENV"
+npm install --save-dev tsx
