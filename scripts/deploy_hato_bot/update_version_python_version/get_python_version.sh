dc04abd160487f8a5b3817ec7a458532f9071563
Masaya Suzuki <suzukimasaya428@gmail.com>
2025-02-20 07:32:58 +0900

    Dockerfileからuvのバージョンを読み取るようにする

--- scripts/deploy_hato_bot/update_version_python_version/get_python_version.sh
+++ scripts/deploy_hato_bot/update_version_python_version/get_python_version.sh
@@ -7,6 +7,3 @@ DOCKER_CMD="python --version 2>&1 | sed -e 's/^Python //g'"
 python_version=$(docker compose run hato-bot sh -c "${DOCKER_CMD}")
 echo "Python version:" "${python_version}"
 sed -i -e "s/requires-python = \"==.*\"/requires-python = \"==${python_version}\"/g" pyproject.toml
-DOCKER_CMD="uv version | sed -e 's/uv \([0-9.]*\)/\1/g'"
-uv_version=$(docker compose run hato-bot sh -c "${DOCKER_CMD}")
-sed -i -e "s/required-version = .*/required-version = \"$uv_version\"/g" pyproject.toml
