0066bdb8df2a6a941f2de43ab2845e53501d37ef
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2025-07-10 07:09:13 +0900

    Update format.sh

--- scripts/pr_format/pr_format/format.sh
+++ scripts/pr_format/pr_format/format.sh
@@ -10,7 +10,3 @@ uv sync --dev
 if [ "$(yq .tool.uv.sources.sudden-death.git pyproject.toml)" != 'null' ]; then
 	uv lock --upgrade-package sudden-death
 fi
-
-uv tool run autopep8 --exit-code --in-place --recursive --exclude pyproject.toml .
-uv tool run pyink --config .python-black --exclude pyproject.toml .
-uv tool run isort --sp .isort.cfg .
