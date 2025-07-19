c8beb82bf302e90616fc1d2611345b86a76bbc72
Masaya Suzuki <suzukimasaya428@gmail.com>
2025-07-19 10:35:37 +0900

    rm unused var

--- scripts/pr_format/pr_format/format.sh
+++ scripts/pr_format/pr_format/format.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 uv run "${GITHUB_WORKSPACE}/scripts/pr_format/pr_format/fix_pyproject.py"
-tag_name="$(yq '.jobs.pr-super-lint.steps[-1].uses' .github/workflows/pr-test.yml | sed -e 's;/slim@.*;:slim;g')"
-tag_version="$(yq '.jobs.pr-super-lint.steps[-1].uses | line_comment' .github/workflows/pr-test.yml)"
 uv sync --dev
 
 if [ "$(yq .tool.uv.sources.sudden-death.git pyproject.toml)" != 'null' ]; then
