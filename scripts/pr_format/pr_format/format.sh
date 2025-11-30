27d6ec8ae38f58bc23f10995281af87c9d894133
masaya.suzuki <suzukimasaya428@gmail.com>
2025-11-30 23:24:01 +0900

    mypyのバージョンをsuper-linterに合わせる

--- scripts/pr_format/pr_format/format.sh
+++ scripts/pr_format/pr_format/format.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 uv run "${GITHUB_WORKSPACE}/scripts/pr_format/pr_format/fix_pyproject.py"
+action="$(yq '.jobs.pr-super-lint.steps[-1].uses | line_comment' .github/workflows/pr-test.yml)"
+mypy_version=$(docker run --rm --entrypoint '' "ghcr.io/super-linter/super-linter:slim-${action}" /bin/sh -c 'mypy --version | sed -e "s/.* \([0-9]*\.[0-9]*\.[0-9]*\) .*/\1/"')
+sed -i -e "s/mypy==[0-9.]*/mypy==${mypy_version}/g" pyproject.toml
 uv sync --dev
 
 if [ "$(yq .tool.uv.sources.sudden-death.git pyproject.toml)" != 'null' ]; then
