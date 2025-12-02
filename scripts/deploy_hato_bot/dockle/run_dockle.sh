21c889944fd2e29e7e8e280d024e0e2143c8aa1e
masaya.suzuki <suzukimasaya428@gmail.com>
2025-12-03 08:24:23 +0900

    ignore settings.py

--- scripts/deploy_hato_bot/dockle/run_dockle.sh
+++ scripts/deploy_hato_bot/dockle/run_dockle.sh
@@ -15,7 +15,7 @@ for image_name in $(docker compose images | awk 'OFS=":" {print $2,$3}' | tail -
 	if [[ "${image_name}" =~ "postgres" ]]; then
 		cmd+="-ak key "
 	elif [[ "${image_name}" =~ "hato-bot" ]]; then
-		cmd+="-i CIS-DI-0006 "
+		cmd+="-i CIS-DI-0006 -af settings.py "
 	fi
 
 	cmd+="${image_name}"
