540092efdadede1e299239d4432a4daf499e0883
masaya.suzuki <suzukimasaya428@gmail.com>
2026-03-11 22:15:46 +0900

    ignore DKL-DI-0005

--- scripts/deploy_hato_bot/dockle/run_dockle.sh
+++ scripts/deploy_hato_bot/dockle/run_dockle.sh
@@ -15,7 +15,7 @@ for image_name in $(docker compose images | awk 'OFS=":" {print $2,$3}' | tail -
 	if [[ "${image_name}" =~ "postgres" ]]; then
 		cmd+="-ak key "
 	elif [[ "${image_name}" =~ "hato-bot" ]]; then
-		cmd+="-i CIS-DI-0006 -af settings.py "
+		cmd+="-i CIS-DI-0006 -i DKL-DI-0005 -af settings.py "
 	fi
 
 	cmd+="${image_name}"
